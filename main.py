ed0b40defcdd941204c4e8220cc0e526c26ffd73
saltapp <815625981@qq.com>
2024-08-07 21:13:57 +0800

    Add page demo

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import json
-from typing import Optional
+from typing import Any, Dict, List, Optional, Union
+import aiohttp
 from dotenv import load_dotenv
+from httpx import AsyncClient
 
+from config import OPENAI_API_BASE_URLS, OPENAI_API_KEYS, AppConfig
 from misc import add_or_update_system_message, get_last_user_message
 from utils import get_rag_context, rag_template
 
@@ -11,10 +14,11 @@ import logging
 import os
 
 import uvicorn
-from fastapi import FastAPI, HTTPException, Request, status
+from fastapi import Body, FastAPI, HTTPException, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import StreamingResponse
+from starlette.background import BackgroundTask
 
 from rag import app as rag_app
 # from app.gradio_ui.ui import gradio_iface
@@ -26,6 +30,10 @@ app = FastAPI(title="RAG x FastAPI")
 
 
 environment = os.getenv("ENVIRONMENT", "dev")  # Default to 'development' if not set
+
+app.state.config = AppConfig()
+app.state.config.OPENAI_API_BASE_URLS = OPENAI_API_BASE_URLS
+app.state.config.OPENAI_API_KEYS = OPENAI_API_KEYS
 
 log = logging.getLogger("uvicorn")
 
@@ -46,162 +54,92 @@ async def get_app_config():
         "version": "demo",
     }
 
-# @app.post("/chat/completions")
-# @app.post("/chat/completions/{url_idx}")
-# async def generate_chat_completion(
-#     form_data: dict,
-#     url_idx: Optional[int] = None,
-# ):
-#     idx = 0
-#     payload = {**form_data}
 
-#     model_id = form_data.get("model")
-#     model_info = Models.get_model_by_id(model_id)
+@app.post("/chat/completions")
+@app.post("/chat/completions/{url_idx}")
+async def generate_chat_completion(
+    form_data: dict,
+    url_idx: Optional[int] = None,
+):
+    idx = 0
+    payload = {**form_data}
 
-#     if model_info:
-#         if model_info.base_model_id:
-#             payload["model"] = model_info.base_model_id
+    if "chat_id" in payload:
+        del payload["chat_id"]
 
-#         model_info.params = model_info.params.model_dump()
+    if "title" in payload:
+        del payload["title"]
 
-#         if model_info.params:
-#             if model_info.params.get("temperature", None) is not None:
-#                 payload["temperature"] = float(model_info.params.get("temperature"))
+    if "task" in payload:
+        del payload["task"]
 
-#             if model_info.params.get("top_p", None):
-#                 payload["top_p"] = int(model_info.params.get("top_p", None))
+    # Convert the modified body back to JSON
+    payload = json.dumps(payload)
 
-#             if model_info.params.get("max_tokens", None):
-#                 payload["max_tokens"] = int(model_info.params.get("max_tokens", None))
+    log.debug(payload)
 
-#             if model_info.params.get("frequency_penalty", None):
-#                 payload["frequency_penalty"] = int(
-#                     model_info.params.get("frequency_penalty", None)
-#                 )
+    url = app.state.config.OPENAI_API_BASE_URLS[0]
+    key = app.state.config.OPENAI_API_KEYS[0]
 
-#             if model_info.params.get("seed", None):
-#                 payload["seed"] = model_info.params.get("seed", None)
+    headers = {}
+    headers["Authorization"] = f"Bearer {key}"
+    headers["Content-Type"] = "application/json"
 
-#             if model_info.params.get("stop", None):
-#                 payload["stop"] = (
-#                     [
-#                         bytes(stop, "utf-8").decode("unicode_escape")
-#                         for stop in model_info.params["stop"]
-#                     ]
-#                     if model_info.params.get("stop", None)
-#                     else None
-#                 )
+    r = None
+    session = None
+    streaming = False
 
-#         system = model_info.params.get("system", None)
-#         if system:
-#             system = prompt_template(
-#                 system,
-#                 **(
-#                     {
-#                         "user_name": user.name,
-#                         "user_location": (
-#                             user.info.get("location") if user.info else None
-#                         ),
-#                     }
-#                     if user
-#                     else {}
-#                 ),
-#             )
-#             # Check if the payload already has a system message
-#             # If not, add a system message to the payload
-#             if payload.get("messages"):
-#                 for message in payload["messages"]:
-#                     if message.get("role") == "system":
-#                         message["content"] = system + message["content"]
-#                         break
-#                 else:
-#                     payload["messages"].insert(
-#                         0,
-#                         {
-#                             "role": "system",
-#                             "content": system,
-#                         },
-#                     )
+    try:
+        session = aiohttp.ClientSession(trust_env=True)
+        r = await session.request(
+            method="POST",
+            url=f"{url}/chat/completions",
+            data=payload,
+            headers=headers,
+        )
 
-#     else:
-#         pass
+        r.raise_for_status()
 
-#     model = app.state.MODELS[payload.get("model")]
-#     idx = model["urlIdx"]
+        # Check if response is SSE
+        if "text/event-stream" in r.headers.get("Content-Type", ""):
+            streaming = True
+            return StreamingResponse(
+                r.content,
+                status_code=r.status,
+                headers=dict(r.headers),
+                background=BackgroundTask(
+                    cleanup_response, response=r, session=session
+                ),
+            )
+        else:
+            response_data = await r.json()
+            return response_data
+    except Exception as e:
+        log.exception(e)
+        error_detail = "Server Connection Error"
+        if r is not None:
+            try:
+                res = await r.json()
+                print(res)
+                if "error" in res:
+                    error_detail = f"External: {res['error']['message'] if 'message' in res['error'] else res['error']}"
+            except:
+                error_detail = f"External: {e}"
+        raise HTTPException(status_code=r.status if r else 500, detail=error_detail)
+    finally:
+        if not streaming and session:
+            if r:
+                r.close()
+            await session.close()
 
-#     if "pipeline" in model and model.get("pipeline"):
-#         payload["user"] = {
-#             "name": user.name,
-#             "id": user.id,
-#             "email": user.email,
-#             "role": user.role,
-#         }
-
-#     # Check if the model is "gpt-4-vision-preview" and set "max_tokens" to 4000
-#     # This is a workaround until OpenAI fixes the issue with this model
-#     if payload.get("model") == "gpt-4-vision-preview":
-#         if "max_tokens" not in payload:
-#             payload["max_tokens"] = 4000
-#         log.debug("Modified payload:", payload)
-
-#     # Convert the modified body back to JSON
-#     payload = json.dumps(payload)
-
-#     log.debug(payload)
-
-#     url = app.state.config.OPENAI_API_BASE_URLS[idx]
-#     key = app.state.config.OPENAI_API_KEYS[idx]
-
-#     headers = {}
-#     headers["Authorization"] = f"Bearer {key}"
-#     headers["Content-Type"] = "application/json"
-
-#     r = None
-#     session = None
-#     streaming = False
-
-#     try:
-#         session = aiohttp.ClientSession(trust_env=True)
-#         r = await session.request(
-#             method="POST",
-#             url=f"{url}/chat/completions",
-#             data=payload,
-#             headers=headers,
-#         )
-
-#         r.raise_for_status()
-
-#         # Check if response is SSE
-#         if "text/event-stream" in r.headers.get("Content-Type", ""):
-#             streaming = True
-#             return StreamingResponse(
-#                 r.content,
-#                 status_code=r.status,
-#                 headers=dict(r.headers),
-#                 background=BackgroundTask(
-#                     cleanup_response, response=r, session=session
-#                 ),
-#             )
-#         else:
-#             response_data = await r.json()
-#             return response_data
-#     except Exception as e:
-#         log.exception(e)
-#         error_detail = "Open WebUI: Server Connection Error"
-#         if r is not None:
-#             try:
-#                 res = await r.json()
-#                 print(res)
-#                 if "error" in res:
-#                     error_detail = f"External: {res['error']['message'] if 'message' in res['error'] else res['error']}"
-#             except:
-#                 error_detail = f"External: {e}"
-#         raise HTTPException(status_code=r.status if r else 500, detail=error_detail)
-#     finally:
-#         if not streaming and session:
-#             if r:
-#                 r.close()
-#             await session.close()
+async def cleanup_response(
+    response: Optional[aiohttp.ClientResponse],
+    session: Optional[aiohttp.ClientSession],
+):
+    if response:
+        response.close()
+    if session:
+        await session.close()
 
 @app.post("/chat/demo_rag")
 async def generate_chat_completion(
@@ -219,7 +157,7 @@ class ChatCompletionMiddleware(BaseHTTPMiddleware):
 
         if request.method == "POST" and any(
             endpoint in request.url.path
-            for endpoint in ["/chat/demo_rag"]
+            for endpoint in ["/chat/demo_rag", "/chat/completions"]
         ):
             log.debug(f"request.url.path: {request.url.path}")
 
@@ -289,6 +227,9 @@ class ChatCompletionMiddleware(BaseHTTPMiddleware):
                 return response
             else:
                 return response
+        # If it's not a chat completion request, just pass it through
+        response = await call_next(request)
+        return response
     
     async def _receive(self, body: bytes):
         return {"type": "http.request", "body": body, "more_body": False}
@@ -307,4 +248,4 @@ app.mount( "/api/rag", rag_app)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host="0.0.0.0", reload=True)
+    uvicorn.run(app="main:app", host="0.0.0.0", port=7000, reload=True)
